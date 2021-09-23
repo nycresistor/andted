b3cf8f10407f1a680ee9d97c8eee63228117de5f
Jonathan Dahan <git@jonathan.is>
2021-09-23 10:21:54 -0400

    First working ping/pong

--- ted.js
+++ ted.js
@@ -1,12 +1,19 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+if (!process.env.BOT_TOKEN) die('Missing BOT_TOKEN in environment')
 
-client.on("ready", () => void console.log("be excellent to each other!"))
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] })
 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!")
+client.on('ready', () => void console.log('be excellent to each other!'))
+
+client.on('message', (message) => {
+  if (message.content.startsWith('ping')) {
+    message.channel.send('pong!')
   }
 })
 
 client.login(process.env.BOT_TOKEN)
+
+function die(message, exit_code=1) {
+  console.error(message)
+  process.exit(exit_code)
+}
