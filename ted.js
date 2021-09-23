5f4904b076ba4f69192008261423e0322ecb5df5
Jonathan Dahan <git@jonathan.is>
2021-09-23 14:33:35 -0400

    Format code

--- ted.js
+++ ted.js
@@ -1,11 +1,16 @@
 if (!process.env.BOT_TOKEN) die('Missing BOT_TOKEN in environment')
 
 const { Client, Intents } = require('discord.js')
-const client = new Client({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] })
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS, // Needed to join a guild
+    Intents.FLAGS.GUILD_MESSAGES // Needed to read guild messages
+  ]
+})
 
 client.on('ready', () => void console.log('be excellent to each other!'))
 
-client.on('message', (message) => {
+client.on('messageCreate', (message) => {
   if (message.content.startsWith('ping')) {
     message.channel.send('pong!')
   }
@@ -13,7 +18,7 @@ client.on('message', (message) => {
 
 client.login(process.env.BOT_TOKEN)
 
-function die(message, exit_code=1) {
+function die (message, exit_code = 1) {
   console.error(message)
   process.exit(exit_code)
 }
